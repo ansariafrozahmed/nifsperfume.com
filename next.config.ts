583acaafa611687f09b43ad7bb06bdb2f403c141
ansariafrozahmed <ansariafroz.py@gmail.com>
2026-09-13 18:26:00 +0530

    Updated

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.1.104'],
   output: "export",
-  
+   
   images: {
+    unoptimized: true,
     // Next 16 allows only [75] by default; quality={100} is coerced without this.
     qualities: [75, 100],
     remotePatterns: [
