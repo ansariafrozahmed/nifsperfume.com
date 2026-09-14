a74a6fd83fbe29776b77b0e5fc8ee40f83191a48
ansariafrozahmed <ansariafroz.py@gmail.com>
2026-09-14 16:45:29 +0530

    Updated

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,7 @@
 export const contact = {
   // digits only, with country code: used for wa.me and tel: links
-  phoneDigits: "918454014703",
-  phoneDisplay: "+91 84540 14703",
+  phoneDigits: "919820664622",
+  phoneDisplay: "+91 98206 64622",
   email: "nifsperfume@gmail.com",
 };
 
